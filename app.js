e3407a0d383a0a56a30f14f4610092f19ce33064
Tychon <tisha.kruglyak@gmail.com>
2022-09-26 16:43:16 +0200

    simple websockets server for chat skeleton

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const http = require('http').Server(app);
+const cors = require('cors');
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+app.use(cors());
+
+let users = [];
+
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on('message', (data) => {
+    console.log(data)
+    socketIO.to(data.socketID).emit('messageResponse', data);
+    socketIO.to(data.sendTo).emit('messageResponse', data);
+  });
+
+  socket.on('newUser', (data) => {
+   
+    users.push(data);
+
+    socketIO.emit('newUserResponse', users);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+    users = users.filter((user) => user.socketID !== socket.id);
+    socketIO.emit('newUserResponse', users);
+    socket.disconnect();
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
